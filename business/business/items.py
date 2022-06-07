fc28cb3bdbabf4c90d90f70230f4b7fb6335d867
Saunee Navneet <navneet.s@simformsolutions.com>
2022-06-07 22:19:54 +0530

    without item loader

--- business/business/items.py
+++ business/business/items.py
@@ -5,16 +5,16 @@
 
 import scrapy
 from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
+from itemloaders.processors import TakeFirst, MapCompose, Join
 from w3lib.html import remove_tags
 
 
 class BusinessItem(scrapy.Item):
     # define the fields for your item here like:
-    biz_name = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
-    address = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
-    contact_name = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
-    contact = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
-    site = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
-    social_links = scrapy.Field()
-    description = scrapy.Field(input_processors=MapCompose(remove_tags), output_processor=TakeFirst())
+    biz_name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    address = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    contact_name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    contact = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    site = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    social_links = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    description = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
--- business/business/spiders/business_spider.py
+++ business/business/spiders/business_spider.py
@@ -13,7 +13,7 @@ class BusinessSpider(scrapy.Spider):
         yield from response.follow_all(business_links, self.parse_biz)
 
     def parse_biz(self, response):
-        item = BusinessItem()
+        # item = BusinessItem()
         def extract_with_get(query):
             return response.css(query).get(default='').strip()
 
@@ -22,18 +22,28 @@ class BusinessSpider(scrapy.Spider):
             return " ".join(_list)
 
         try:
+            # from scrapy.shell import inspect_response
+            # inspect_response(response, self)
             all_biz_det = response.css("div.tabber")
-            if all_biz_det.css('h2 span a'):
-                social_list = all_biz_det.css("div.ListingDetails_Level5_SOCIALMEDIA a::attr(href)").getall()
-                social_list = [urllib.parse.unquote(each_link.split("?URL=")[-1]) for each_link in social_list]
-                item["biz_name"] = extract_with_get("h2 span a::text")
-                item["address"] = extract_with_getall("span[itemprop]::text")
-                item["contact_name"] = extract_with_get("span.ListingDetails_Level5_MAINCONTACT a::text")
-                item["contact"] = all_biz_det.css("span.ListingDetails_Level5_MAINCONTACT::text").getall()[-1]
-                item["site"] = extract_with_get("a.ListingDetails_Level5_SITELINK::attr(href)")
-                item["social_links"] = social_list
-                item["description"] = extract_with_get("div.ListingDetails_Level5_DESCRIPTION p::text")
-                yield item
+            if all_biz_det.css("h2 span a"):
+                my_loader = ItemLoader(item=BusinessItem(), selector=all_biz_det)
+                my_loader.add_css("biz_name", "h2 span a")
+                my_loader.add_css("address", "span[itemprop]")
+            # my_loader.add_css("contact_name", "span.ListingDetails_Level5_MAINCONTACT a")
+            # my_loader.add_css("contact", "span.ListingDetails_Level5_MAINCONTACT")
+            # my_loader.add_css("site", "a.ListingDetails_Level5_SITELINK::attr(href)")
+            # my_loader.add_css("social_links", "div.ListingDetails_Level5_SOCIALMEDIA a::attr(href)")
+            # my_loader.add_css("description", "div.ListingDetails_Level5_DESCRIPTION p")
+            # social_list = all_biz_det.css("div.ListingDetails_Level5_SOCIALMEDIA a::attr(href)").getall()
+            # social_list = [urllib.parse.unquote(each_link.split("?URL=")[-1]) for each_link in social_list]
+            # item["biz_name"] = extract_with_get("h2 span a::text")
+            # item["address"] = extract_with_getall("span[itemprop]::text")
+            # item["contact_name"] = extract_with_get("span.ListingDetails_Level5_MAINCONTACT a::text")
+            # item["contact"] = all_biz_det.css("span.ListingDetails_Level5_MAINCONTACT::text").getall()[-1]
+            # item["site"] = extract_with_get("a.ListingDetails_Level5_SITELINK::attr(href)")
+            # item["social_links"] = social_list
+            # item["description"] = extract_with_get("div.ListingDetails_Level5_DESCRIPTION p::text")
+                yield my_loader.load_item()
             # elif all_biz_det.css('h2 span span'):
             #     social_list = all_biz_det.css("div.ListingDetails_Level5_SOCIALMEDIA a::attr(href)").getall()
             #     social_list = [urllib.parse.unquote(each_link.split("?URL=")[-1]) for each_link in social_list]
@@ -47,6 +57,6 @@ class BusinessSpider(scrapy.Spider):
             #         "description": extract_with_get("div.ListingDetails_Level5_DESCRIPTION p::text")
             #     }
             biz_links = all_biz_det.css('a.level1_footer_left_box_a.friendly')[0:2]
-            yield from response.follow_all(biz_links,  self.parse_biz)
+            yield from response.follow_all(biz_links, self.parse_biz)
         except Exception as err:
             self.logger.exception(err)
